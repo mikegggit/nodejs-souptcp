732c7fa86df21b6d3c517866bc98ff4a868caf53
Michael Grudkowski <mgrudkowski@gmail.com>
2019-01-26 22:45:14 -0500

    WIP: cleanup for mikegggit  - doc

--- client.js
+++ client.js
@@ -2,12 +2,17 @@ var net = require('net');
 var util = require('util');
 var os = require('os');
 
-
-var TIMEOUT_HEARTBEAT_IN_SECONDS = 1;
+var START_SEQNO                            = 1;
+var UNAME                                  = "brace1";
+var PASSWORD                               = "password10";
+var SESSIONID                              = "";
+var SEQNO                                  = "1";
+var TIMEOUT_HEARTBEAT_IN_SECONDS           = 1;
 var TIMEOUT_NO_SERVER_HEARTBEAT_IN_SECONDS = 15;
-var serverMonitor;
+var LOGIN_PACKET                           = "L" + UNAME + PASSWORD + SESSIONID.padStart(10, " ") + SEQNO.padStart(10, " ") + os.EOL;
 
-var parseArgs=require('minimist')
+var serverMonitor;
+var parseArgs                              = require('minimist')
 
 result = parseArgs(process.argv.slice(2) , 
   {"opts.boolean": true})
@@ -25,9 +30,13 @@ process.on('exit', function(code) {
 
 var isDebug = result['debug'];
 var sessionid = result['sessionid'];
-console.log(isDebug);
-console.log(sessionid);
 
+/**
+ * SoupClient constructor.
+ * @constructor
+ *
+ * @param config
+ */
 function SoupClient(config) {
   _this = this;
   this.sessionid = config['sessionid'];
@@ -35,25 +44,40 @@ function SoupClient(config) {
   this.socket;
   this.heartbeat;
   this.serverMonitor;
+  
+  /**
+   * Timer invoked periodically to send client heartbeat to the server to maintain the connection, letting the server know we're still here.
+   */
   this.scheduleClientHeartbeat = function() {
     this.heartbeat = setInterval(function() {
       _this.socket.write('R\n');
     }, TIMEOUT_HEARTBEAT_IN_SECONDS * 1000);
   }; 
+ 
+  /**
+   * Timer that waits for a server heartbeat within some interval.  If nothing is heard, client assumes the connection has been severed and explicitly severs it from the client side.
+   */
   this.scheduleServerHeartbeatMonitor = function() {
     this.serverMonitor = setTimeout(function() {
       _this.socket.end("No server detected...Terminating connection.");
     } , TIMEOUT_NO_SERVER_HEARTBEAT_IN_SECONDS * 1000);
   };
+ 
+  /**
+   * Invoked to connect to the server socket.
+   *
+   * Establishes a socket, along with callbacks on the socket to handle connection issues and data received from the server.
+   */
   this.connect = function() {
     this.socket = net.connect({port: this.port},
       function() { //'connect' listener
 	console.log('client connected');
-	_this.socket.write('Lbrace1password10                              ' + os.EOL);
+	_this.socket.write(LOGIN_PACKET);
       }
     );
     this.socket.on('error', function(e) {
       util.log("Error");
+      socket.end()
     });
     this.socket.on('data', function(data) {
       util.log("heard data")
@@ -73,6 +97,11 @@ function SoupClient(config) {
 	clearTimeout(_this.serverMonitor);
 	_this.scheduleServerHeartbeatMonitor();
 	break;
+      case 'S':
+        util.log("heard sequenced message." + data.toString("utf-8"));
+	clearTimeout(_this.serverMonitor);
+	_this.scheduleServerHeartbeatMonitor();
+        break; 
       }
     });
   }
--- example/client.js
+++ example/client.js
@@ -4,6 +4,7 @@ var config = {
   port: 9000
 };
 
+// We don't pass a sessionid, so we'll connect to whatever the active session is on the server.
 var client = new SoupClient(config);
 client.connect();
 
